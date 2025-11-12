6fc0bd27c4582145e6847d84ea16ed98786ebbf8
ajitzz <ajithsabzz@gmail.com>
2025-11-12 16:17:23 +0530

    Rental2 Section Cleared

--- components/salary/Salary.tsx
+++ components/salary/Salary.tsx
@@ -1,5 +1,5 @@
 import s from './Salary.module.css';
-
+import Collapsible from '@/components/Collapsible/Collapsible';
 export default function Salary() {
   return (
     <section id="salary" className={s.section} aria-label="Salary Plan">
@@ -14,15 +14,8 @@ export default function Salary() {
         </div>
 
         <div className={s.text}>
-          <h2 className={s.h2}>SALARY PLAN</h2>
-          <div className={s.scroll}>
-            <p>
-              I&#39;m a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. I&#39;m a great place for you to tell a story and let your users know a little more about you.
-            </p>
-          </div>
-          <a className={`${s.btn} ${s.btnLight}`} href="#apply">
-            Read More <i className="fa-solid fa-arrow-right" />
-          </a>
+          <h2 className={s.h2}>OUR SUPPORT</h2>
+         <Collapsible/>
         </div>
       </div>
     </section>
